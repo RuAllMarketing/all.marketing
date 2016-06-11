90dce47ccad5a5300efa35ff380beb040648d1a7
Victor Stone <info@victorstone.ru>
2016-06-11 19:22:34 +0300

    up to v0.01

---
+++ public/admin/assets/js/config.ajax-constant.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * Config constant
+ */
+app.constant('AJAX_URLS', {
+    'admin-login': 'http://all.marketing/admin-login',
+    'admin-list-all-property': 'http://all.marketing/list-all-property',
+    'admin-delete-property': 'http://all.marketing/delete-property',
+    'admin-approve-property': 'http://all.marketing/approve-property',
+    'admin-decline-property': 'http://all.marketing/decline-property'
+});
+
---
+++ public/assets/js/config.ajax-constant.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**
+ * Config constant
+ */
+app.constant('AJAX_URLS', {
+    'user-register': 'http://all.marketing/register-user',
+    'user-activate': 'http://all.marketing/activate-user/%hash%',
+    'user-login':  'http://all.marketing/user-login',
+    'list-user-property':  'http://all.marketing/list-user-property',
+    'add-user-property':  'http://all.marketing/add-user-property',
+    'update-user-property':  'http://all.marketing/update-user-property',
+    'upload-property-images':  'http://all.marketing/upload-images-for-property',
+    'list-top-property':  'http://all.marketing/top-property-list'
+});
+
